67880489808900a2c8b1f50f0ca5701370b3c21b
janttuu <janina.nietula@gmail.com>
2026-02-04 15:05:58 +0200

    tehtava2.1

---
+++ moduuli 2/tehtava2.3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna kanta:"))
+korkeus = int(input("Anna korkeus:"))
+
+piiri = 2 * (kanta * korkeus)
+pinta_ala = kanta * korkeus
+
+print("Suorakulmion piiri on", piiri)
+print("Suorakulmion pinta_ala on", pinta_ala)
