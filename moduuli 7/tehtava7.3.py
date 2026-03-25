47bc0bffe9842b93826402d52b84eea5baf31bf9
janttuu <janina.nietula@gmail.com>
2026-03-25 16:09:14 +0200

    valmis

---
+++ moduuli 7/tehtava7.3.py
@@ -0,0 +1,29 @@
+lentoasemat = {}
+
+while True:
+    print("\nValitse toiminto:")
+    print("1 = Syötä uusi lentoasema")
+    print("2 = Hae lentoaseman tiedot")
+    print("3 = Lopeta")
+
+    valinta = input("Anna valinta: ")
+
+    if valinta == "1":
+        icao = input("Anna lentoaseman ICAO-koodi: ").upper()
+        nimi = input("Anna lentoaseman nimi: ")
+        lentoasemat[icao] = nimi
+        print("Lentoasema tallennettu.")
+
+    elif valinta == "2":
+        icao = input("Anna haettava ICAO-koodi: ").upper()
+        if icao in lentoasemat:
+            print("Lentoaseman nimi on:", lentoasemat[icao])
+        else:
+            print("ICAO-koodia ei löytynyt.")
+
+    elif valinta == "3":
+        print("Ohjelma lopetetaan.")
+        break
+
+    else:
+        print("Virheellinen valinta.")
